0e5bfa350aec524df844f021a3322145ffad5d0f
abdelrahman1451255 <128540318+abdelrahman1451255@users.noreply.github.com>
2023-03-29 13:41:25 +0300

    Add files via upload

---
+++ 2ndHomeWork/2ndHomeWork/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _2ndHomeWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ok = new System.Windows.Forms.Button();
+            this.Ex = new System.Windows.Forms.Button();
+            this.Dis = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(60, 60);
+            this.ok.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(50, 50);
+            this.ok.TabIndex = 0;
+            this.ok.Text = "Ok";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Visible = false;
+            this.ok.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ex
+            // 
+            this.Ex.Location = new System.Drawing.Point(150, 60);
+            this.Ex.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Ex.Name = "Ex";
+            this.Ex.Size = new System.Drawing.Size(50, 50);
+            this.Ex.TabIndex = 1;
+            this.Ex.Text = "Exit";
+            this.Ex.UseVisualStyleBackColor = true;
+            this.Ex.Click += new System.EventHandler(this.Ex_Click);
+            // 
+            // Dis
+            // 
+            this.Dis.Enabled = false;
+            this.Dis.Location = new System.Drawing.Point(109, 125);
+            this.Dis.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Dis.Name = "Dis";
+            this.Dis.Size = new System.Drawing.Size(50, 50);
+            this.Dis.TabIndex = 2;
+            this.Dis.Text = "Disable";
+            this.Dis.UseVisualStyleBackColor = true;
+            this.Dis.Click += new System.EventHandler(this.Dis_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 187);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "change Back";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(160, 187);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "hides/shows";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(33, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "welcome to project 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Dis);
+            this.Controls.Add(this.Ex);
+            this.Controls.Add(this.ok);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimumSize = new System.Drawing.Size(100, 100);
+            this.Name = "Form1";
+            this.Text = "worksheet1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button Ex;
+        private System.Windows.Forms.Button Dis;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 2ndHomeWork/2ndHomeWork/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2ndHomeWork
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hellowrod");
+        }
+
+        private void Dis_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            i++;
+            if (i % 2 != 0)
+            {
+                label1.Visible = false;
+            }
+            else
+            {
+                label1.Visible = true;
+            }
+        }
+
+        private void Ex_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
